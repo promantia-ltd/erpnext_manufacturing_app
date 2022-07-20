4d12908a2d6aff71331524bc67c912385c7df4c6
vijaynaidu123 <vijay.n@promantia.com>
2022-07-20 12:39:28 +0530

    12324/commened override_whitelisted_methods in hooks.py

--- manufacturer_customizations/manufacturer_customizations/doctype/quality_inspection/quality_inspection.py
+++ manufacturer_customizations/manufacturer_customizations/doctype/quality_inspection/quality_inspection.py
@@ -3,6 +3,6 @@ from frappe import _
 def on_submit(doc,method):
     accepted_qty = doc.quantity_passed
     frappe.db.set_value("Job Card Time Log",{'parent':doc.reference_name}, 'accepted_qty',accepted_qty)
-    frappe.db.set_value("Work Order",doc.reference_name,'final_accepted_qty',accepted_qty)
+    frappe.db.set_value("Work Order",doc.reference_name,'final_qty',accepted_qty)
     if doc.quantity_tested < doc.quantity_passed:
         frappe.throw("Quantity Passed cannot be greater than Quantity Tested")
