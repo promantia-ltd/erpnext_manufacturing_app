a1be9d95e7a54c000fc7121aad4b80f52401dbc3
IshwaryaM3 <ishwarya.m@promantia.com>
2022-09-05 12:38:07 +0530

    [13366/Deletion & Updatation of the Fields in the JOB card]Adding the job card timelog field calculation changes

--- manufacturer_customizations/manufacturer_customizations/doctype/quality_inspection/quality_inspection.py
+++ manufacturer_customizations/manufacturer_customizations/doctype/quality_inspection/quality_inspection.py
@@ -2,13 +2,28 @@ import frappe
 from frappe import _
 
 def on_submit(doc,method=None):
-    accepted_qty = doc.quantity_passed
-    frappe.db.set_value("Job Card Time Log",{'parent':doc.reference_name}, 'accepted_qty',accepted_qty)
+    accepted_qty = int(doc.quantity_passed)
+    # frappe.db.set_value("Job Card Time Log",{'parent':doc.reference_name}, 'accepted_qty',accepted_qty)
     frappe.db.set_value("Work Order",doc.reference_name,'final_qty',accepted_qty)
     if doc.quantity_passed and doc.quantity_tested:
         if int(doc.quantity_passed) > int(doc.quantity_tested):
             frappe.throw("Quantity Passed cannot be greater than Quantity Tested")
-
+    excess_qty=0
+    shortfall_qty=0
     if doc.reference_type=="Job Card" and doc.reference_name:
         record=frappe.get_doc(doc.reference_type,doc.reference_name)
+        if record.time_logs:
+            for each in record.time_logs:
+                each.accepted_qty=int(accepted_qty)
+                each.rejected_qty=int(each.launch_qty) - int(each.accepted_qty)
+                if(int(each.accepted_qty)>int(each.order_qty)):
+                    excess_qty = int(each.accepted_qty) - int(each.order_qty)
+                elif(int(each.accepted_qty)<=int(each.order_qty)):
+                    excess_qty=0
+                if(int(each.accepted_qty)<int(each.order_qty)):
+                    shortfall_qty=int(each.order_qty)-int(each.accepted_qty)
+                elif(int(each.accepted_qty)>=int(each.order_qty)):
+                    shortfall_qty=0
+                each.excess_qty=excess_qty
+                each.shortfall_qty=shortfall_qty
         record.submit()
